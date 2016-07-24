5c22e149506c7898db6ed77cf8d9fd224c209fb2
= <chu.andy1993@gmail.com>
2016-07-24 08:56:24 -0700

    first commit

---
+++ Routes.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "net/http"
+)
+
+type Route struct {
+    Name    string
+    Method  string
+    Pattern string
+    HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+
+var routes = Routes{
+    Route{
+        "Index",
+        "GET",
+        "/",
+        Index,
+    },
+    Route{
+        "TodoIndex",
+        "GET",
+        "/todos",
+        TodoIndex,
+    },
+    Route{
+        "TodoShow",
+        "GET",
+        "/todos/{todoId}",
+        TodoShow,
+    },
+    Route{
+        "TodoCreate",
+        "POST",
+        "/todos",
+        TodoCreate,
+    },
+    Route{
+        "IntervalHistory",
+        "GET",
+        "/intervalhistory/{start}/{end}",
+        IntervalHistory,
+    },
+}
